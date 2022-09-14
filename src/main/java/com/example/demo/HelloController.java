e1ab896ef31c6ea28a20f5ec89c438150c65e61c
cemetry <matvey.marusik@mail.ru>
2022-09-14 23:56:18 +0300

    prog

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class HelloController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button buttonForward;
+
+    @FXML
+    void initialize() {
+
+    }
+
+}
